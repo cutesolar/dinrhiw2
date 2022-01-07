7ef3ccc8a9adf93696ef76b8ddf93a96e3b31f96
Tomas Ukkonen <tomas.ukkonen@iki.fi>
2022-01-07 22:09:49 +0200

    Mini update.

--- tools/aescipher.cpp
+++ tools/aescipher.cpp
@@ -10,15 +10,9 @@
  * aescipher returns non-zero if there was errors.
  * if file were crypted/decrypted correctly it returns zero.
  * 
- * missing features:
- *  - delete files '-D' flag (deletes&shreds original/encrypted file)
- *  - '-p <passphrase>' based encryption (SHA-1 given passphrase)
- *  - write test scripts to best binary (all cases) so that no
- *    data is lost (if key is known)
- *  - add compression to CryptoFileSource (gzip/libz is easy to use)
- *  - keyfiles
- *  - create symmetric keyfile
- *
+ * TODO 2021:
+ *  - add support for UNICODE filenames and directories
+ * 
  * improvements:
  *  - change fileformat to be:
  *    ID, IV, CONTENT, PAD. add always random PAD 
@@ -72,7 +66,8 @@ int main(int argc, char** argv)
     
     CryptoFileSource<128>* filesource;
     bool encryptData = true;
-    unsigned int numrounds = 11;
+    // unsigned int numrounds = 11; // AES-128 uses 10 rounds [+1 for initial round key]
+    unsigned int numrounds = 15; // AES-256 uses 14 rounds [+1 for initial round key]
     
     bool verbose = false;
     bool aarmour = false;
@@ -86,7 +81,7 @@ int main(int argc, char** argv)
 		      numrounds, aarmour,
 		      verbose, mode) == false)
     {
-      std::cout << "AES-128 cipher <https://github.com/cslr/dinrhiw2/>" << std::endl;
+      std::cout << "AES-256 cipher <https://github.com/cslr/dinrhiw2/>" << std::endl;
       std::cout << "usage: aescipher [-rN] [-d] [-v] [-mode] secredpassphrase filename" << std::endl;
       std::cout << "       mode = ecb, cfb, cbc, ofb, ctr. (default: ctr)" << std::endl;
       return -2;
@@ -96,7 +91,7 @@ int main(int argc, char** argv)
     
     
     if(verbose){
-      std::cout << "AES-128 ";
+      std::cout << "AES-256 ";
       if(mode == crypto::ECBmode) std::cout << "ECB";
       else if(mode == crypto::CFBmode) std::cout << "CFB";
       else if(mode == crypto::CBCmode) std::cout << "CBC";
@@ -112,6 +107,7 @@ int main(int argc, char** argv)
     cipher = new whiteice::crypto::AES();
     keyschedule = new whiteice::crypto::AESKey(key);
     filesource = new CryptoFileSource<128>(filename, encryptData, false);
+    auto original_filename = filename;
 
 
     if(filesource->good() == false){
@@ -147,14 +143,11 @@ int main(int argc, char** argv)
 	rv = - 1;
       }
       else{
-	auto original_filename = filename;
 	create_encrypted_filename(filename);
 	
 	if(filesource->write(filename)){
 	  if(verbose)
 	    std::cout << "ok." << std::endl;
-
-	  secure_delete(original_filename);
 	}
 	else{
 	  std::cerr << "file writing (i/o) failure." << std::endl;
@@ -173,14 +166,12 @@ int main(int argc, char** argv)
 	rv = -1;
       }
       else{
-	auto original_filename = filename;
 	create_decrypted_filename(filename);
 	
 	if(filesource->write(filename)){
 	  if(verbose)
 	    std::cout << "ok." << std::endl;
-
-	  secure_delete(original_filename);
+	  
 	}
 	else{
 	  std::cerr << "file writing (i/o) failure." << std::endl;
@@ -195,6 +186,10 @@ int main(int argc, char** argv)
     delete cipher;
     delete keyschedule;
     delete filesource;
+    
+    if(secure_delete(original_filename) == false){
+      std::cout << "Deletion of input file FAILED." << std::endl;
+    }
     
     return rv;
   }
@@ -272,18 +267,24 @@ bool secure_delete(const std::string& filename)
 
     if(byteswritten == 0){
       close(fd);
+      free(block);
       return false;
     }
   }
 
   if(write(fd, block, filesize) != filesize){
     close(fd);
+    free(block);
     return false;
   }
 
   close(fd);
+  free(block);
 
-  remove(filename.c_str());
+  if(remove(filename.c_str()) != 0){
+    std::cout << "DEBUG: remove() failed: " << filename.c_str() << std::endl;
+    return false;
+  }
   
   return true;
 }
@@ -351,15 +352,15 @@ bool read_arguments(unsigned int argc, char** argv,
     else
       key = whiteice::math::integer( std::string(argv[argc-2]), 16 );
     
-    key.resize(128);
+    key.resize(256);
   }
   else{
     // intepretes argv[argc-2] as passphrase
     
-    whiteice::crypto::SHA sha(256); // uses only lowest 128bits
+    whiteice::crypto::SHA sha(256); // uses full 256 bits as the key
     unsigned char hash[(256/8)];
     
-    key.resize(128);
+    key.resize(256);
     unsigned char* ptr = 
       (unsigned char*)malloc(strlen(argv[argc-2])+1);
     
@@ -368,7 +369,7 @@ bool read_arguments(unsigned int argc, char** argv,
     if(!sha.hash(&ptr, strlen((char*)ptr), hash))
       return false;
     
-    for(unsigned int i=0;i<(256/8)/2;i++)
+    for(unsigned int i=0;i<(256/8);i++)
       key.value(i) = hash[i];
     
     if(verbose)
