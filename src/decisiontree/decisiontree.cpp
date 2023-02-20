150bae7addb3f1270914cfcae62156a5a2b4b4a0
Tomas Ukkonen <tomas.ukkonen@iki.fi>
2023-02-20 23:38:05 +0200

    decision tree initial code, needs fixing to make compile and fix bugs.

--- src/decisiontree/decisiontree.cpp
+++ src/decisiontree/decisiontree.cpp
@@ -7,44 +7,439 @@ namespace whiteice
 {
 
   
-  DecisionTree::DecisionTree(const unsigned int NUM_BIN_INPUTS,
-			     const unsigned int NUM_DISCRETE_OUTCOMES)
+  DecisionTree::DecisionTree()
   {
-  }
-  
-  DecisionTree::~DecisionTree(){
+    running = false;
+    worker_thread = nullptr;
+    inputs = nullptr;
+    outcomes = nullptr;
     
+    {
+      std::lock_guard<std::mutex> lock(tree_mutex);
+      tree = nullptr;
+    }
   }
 
   
-  bool DecisionTree::startTrain(const std::vector< std::vector<bool> >& input, const std::vector< std::vector<bool> >& outcomes)
+  DecisionTree::~DecisionTree()
   {
+    stopTrain();
+
+    {
+      std::lock_guard<std::mutex> lock(tree_mutex);
+
+      if(tree){
+	tree->deleteChilds();
+	delete tree;
+      }
+      
+      tree = nullptr;
+    }
+  }
+
+  
+  bool DecisionTree::startTrain(const std::vector< std::vector<bool> >& inputs,
+				const std::vector< std::vector<bool> >& outcomes)
+  {
+    if(running) return false;
+
+    {
+      std::lock_guard<std::mutex> lock(thread_mutex);
+
+      if(running) return false;
+    
+      this->inputs = &inputs;
+      this->outcomes = &outcomes;
+
+      // clears tree structure [FIXME creation of thread may fail so we lose tree data structure]
+      {
+	std::lock_guard<std::mutex> lock(tree_mutex);
+	if(tree){
+	  tree->deleteChilds();
+	  delete tree;
+	}
+	
+	tree = nullptr;
+      }
+
+      try{
+	running = true;
+	worker_thread = new std::thread(DecisionTree::worker_thread_loop);
+
+	return running;
+      }
+      catch(){
+	if(worker_thread) delete worker_thread;
+	worker_thread = nullptr;
+	running = false;
+	return false;
+      }
+      
+    }
+      
     return false;
   }
   
   bool DecisionTree::stopTrain()
   {
+    if(running == false) return false;
+
+    {
+      std::lock_guard<std::mutex> lock(thread_mutex);
+
+      if(running == false) return false;
+
+      running = false;
+
+      if(worker_thread){
+	worker_thread->join();
+	delete worker_thread;
+	worker_thread = nullptr;
+      }
+
+      inputs = nullptr;
+      outcomes = nullptr;
+
+      return true;
+    }
+    
     return false;
   }
+
   
   bool DecisionTree::isRunning() const
   {
-    return false;
+    std::lock_guard<std::mutex> lock(thread_mutex);
+
+    if(running) return true;
+    else return false;
   }
   
+
+  // classify input to target class of the most active variable in outcomes
   unsigned int DecisionTree::classify(std::vector<bool>& input) const
   {
-    return false;
+    DTNode* current;
+
+    while(current->decisionVariable > 0){
+      if(input[current->decisionVariable] == false){
+	if(current->left0) current = current->left0;
+	else return current->outcome;
+      }
+      else if(input[current->decisionVariable] == true){
+	if(current->right0) current = current->right0;
+	else return current->outcome;
+      }
+    }
+
+    return current->outcome;
   }
+
   
   bool DecisionTree::save(const std::string& filename) const
   {
-    return false;
+    // for each node we save following information:
+    // NODEID, DECISION_VARIABLE, OUTCOME_VARIABLE, LEFT0_NODEID, RIGHT0_NODEID
+    // there are all int variables (2**31 values)
+
+    std::vector<int> data;
+
+    {
+      std::lock_guard<std::mutex> lock(tree_mutex);
+      
+      int counter = 0;
+      
+      {
+	std::mutex counter_mutex;
+	
+	tree->calculateNodeIDs(counter_mutex, counter);
+      }
+      
+      
+      data.resize(counter*5);
+      
+      if(tree->saveData(data) == false)
+	return false;
+    }
+
+    // saves data vector to disk
+
+    FILE* handle = fopen(filename.c_str(), "wb");
+    if(handle == NULL) return false;
+    
+    if(fwrite(handle, sizeof(int), data.size(), data.data()) != data.size()){
+      fclose(handle);
+      return false;
+    }
+
+    fclose(handle);
+    
+    return true;
   }
+
+
+#include <sys/stat.h>
+  
+  long long GetFileSize(std::string& filename)
+  {
+    struct stat stat_buf;
+    int rc = stat(filename.c_str(), &stat_buf);
+    return rc == 0 ? stat_buf.st_size : -1;
+  }
+  
   
   bool DecisionTree::load(const std::string& filename)
   {
-    return false;
+    // loads file to data vector
+    std::vector<int> data;
+
+    // gets filesize and resize vector accordingly
+    long long filesize = GetFileSize(filename);
+    if(filesize % sizeof(int) != 0) return false;
+
+    data.resize(filesize / sizeof(int));
+
+    // reads data from disk
+    FILE* handle = fopen(filename.c_str(), "rb");
+    if(handle == NULL) return false;
+
+    if(fread(handle, sizeof(int), data.size(), data.data()) != data.size()){
+      fclose(handle);
+      return false;
+    }
+
+    fclose(handle);
+    
+    // recreates tree structure from data:
+    // NODEID, DECISION_VARIABLE, OUTCOME_VARIABLE, LEFT0_NODEID, RIGHT0_NODEID
+    // there are all int variables (2**31 values)
+
+    DTNode* node;
+
+    if(node->loadData(std::vector<int>& data, 0) == false){
+
+      node->deleteChilds();
+      
+      return false;
+    }
+
+
+    {
+      std::lock_guard<std::mutex> lock(tree_mutex);
+      
+      if(tree){
+	tree->deleteChilds();
+	delete tree;
+      }
+      
+      tree = node;
+    }
+    
+    return true;
+  }
+
+
+
+  // returns true if node's parent's variables matches to data
+  bool DecisionTree:matchData(const DTNode* n, const std::vector<bool>& data) const{
+
+    while(n->parent != NULL){
+
+      if(n->parent->left0 == n){
+	// n->parent->decisionVariable == 0
+	if(data[n->parent->decisionVariable] != false) return false;
+      }
+      else if(n->parent->right1 == n){
+	// n->parent->decisionVariable == 1
+	if(data[n->parent->decisionVariable] != true) return false;
+      }
+
+      n = n->parent;
+    }
+
+    return true;
+  }
+  
+
+  bool DecisionTree::calculateGoodnessSplit(const DTNode* n,
+					    int& split_variable, float& split_goodness, int& node_outcome) const
+  {
+    if(n == NULL) return false;
+    if(n->variableSet.size() == 0) return false;
+
+    int best_variable = -1;
+    float best_goodness = -1.0f;
+    int best_outcome = -1;
+    
+    
+    for(auto& candidateSplit : n->variableSet){
+
+      std::set<unsigned long long> rows0; // data rows where variable is 0 and parent nodes are as set
+      std::set<unsigned long long> rows1; // data rows where variable is 1 and parent nodes are as set
+
+      for(unsigned long long i=0;i<inputs->size();i++){
+	if(matchData(n, (*inputs)[i])){ // checks if node's variable selection matches row
+	  if((*inputs)[i][candidateSplit] == false) rows0.insert(i);
+	  else rows1.insert(i);
+	}
+      }
+
+      // calculates GINI index for the data rows
+      
+      const float weight0 = rows0.size() / (float)(rows0.size() + rows1.size());
+      const float weight1 = rows1.size() / (float)(rows0.size() + rows1.size());
+
+      // calculates p-values for outcomes rows
+      std::vector<float> p0, p1;
+
+      p0.resize((*outcomes)[0].size());
+      p1.resize((*outcomes)[0].size());
+
+      for(auto& p : p0) p = 0.0f;
+      
+      for(auto& r : rows0){
+	for(unsigned int k=0;k<p0.size();k++)
+	  if((*outcomes)[r][k]) p0[k]++;
+      }
+      
+      for(auto& p : p0) p /= (float)rows0.size();
+
+      for(auto& p : p1) p = 0.0f;
+
+      for(auto& r : rows1){
+	for(unsigned int k=0;k<p1.size();k++)
+	  if((*outcomes)[r][k]) p1[k]++;
+      }
+      
+      for(auto& p : p1) p /= (float)rows1.size();
+
+      // GINI value is gini = 1 - ||p||^2 is used for estimating splitting goodness
+
+      float g0 = 0.0f;
+      
+      for(auto& p : p0) g0 += p*p;
+
+      g0 = 1.0f - g0;
+
+      float g1 = 0.0f;
+      
+      for(auto& p : p1) g1 += p*p;
+
+      g1 = 1.0f - g1;
+
+
+      const float GINI = weight0*g0 + weight1*g1;
+
+      if(GINI > best_goodness){
+	best_goodness = GINI;
+	best_variable = candidateSplit;
+
+	// calculate outcome [p-values of current node]
+	std::vector<float> pfull;
+	
+	pfull0.resize((*outcomes)[0].size());
+	
+	for(auto& pfull : p0) p = 0.0f;
+      
+	for(auto& r : rows0){
+	  for(unsigned int k=0;k<p0.size();k++)
+	    if((*outcomes)[r][k]) p0[k]++;
+	}
+	
+	for(auto& r : rows1){
+	  for(unsigned int k=0;k<pfull.size();k++)
+	    if((*outcomes)[r][k]) pfull[k]++;
+	}
+	
+	for(auto& p : pfull) p /= (float)(rows0.size() + rows1.size());
+
+	float pbest = pfull[0];
+	int pindex = 0;
+
+	for(int i=0;i<pfull.size();i++){
+	  if(pbest > pfull[i]){
+	    pbest = pfull[i];
+	    pindex = i;
+	  }
+	}
+	
+	best_outcome = pindex;
+      }
+      
+    }
+
+    split_variable = best_variable;
+    split_goodness = best_goodness;
+    node_outcome = best_outcome;
+
+    return (split_variable >= 0); // found split variable
+  }
+  
+  
+  void DecisionTree::worker_thread_loop()
+  {
+    if(running == false) return;
+
+    
+    std::lock_guard<std::mutex> lock(tree_mutex);
+    
+    tree = new DTNode(); 
+
+    DTNode* current = tree;
+    std::map<DTNode*, float> goodness;
+    std::set<int> initialVariableSet;
+
+    for(unsigned int i=0;i<inputs.size();i++)
+      initialVaribleSet.insert((int)i);
+    
+    int var = -1;
+    int outcome = -1;
+    float g = 0.0;
+    
+    current->variableSet = initialVariableSet;
+
+    if(calculateGoodnessSplit(current, var, g) == true){
+      current->decisionVariable = var;
+      goodness.insert(std::pair<DTNode*,float>(current, g));
+    }
+
+    
+    while(goodness.size() > 0 && running){
+      
+      auto iter = goodness.end();
+      iter--;
+      current = iter->first;
+      goodness.erase(iter);
+
+      // now split based on variable
+
+      DTNode* left0 = new DTNode();
+      DTNode* right1 = new DTNode();
+
+      left0->parent = current;
+      right1->parent = current;
+      left0->variableSet = current->variableSet;
+      right1->variableSet = current->variableSet;
+
+      left0->variableSet.erase(current->decisionVariable);
+      right1->variableSet.erase(current->decisionVariable);
+
+      if(calculateGoodnessSplit(left0, var, g, outcome) == true){
+	left0->decisionVariable = var;
+	left0->outcome = outcome;
+	current->left0 = left0;
+	goodness.insert(std::pair<DTNode*,float>(left0, g));
+      }
+      
+      if(calculateGoodnessSplit(right1, var, g, outcome) == true){
+	right1->decisionVariable = var;
+	right1->outcome = outcome;
+	current->right1 = right1;
+	goodness.insert(std::pair<DTNode*,float>(right1, g));
+      }
+
+    }
+
   }
   
   
--- src/decisiontree/decisiontree.h
+++ src/decisiontree/decisiontree.h
@@ -10,17 +10,114 @@
 #include <vector>
 #include <string>
 
+#include <mutex>
+#include <thread>
 
 namespace whiteice
 {
+  class DTNode
+  {
+  public:
+    DTNode(){
+      decisionVariable = -1;
+      outcome = -1;
+      nodeid = -1;
+      
+      parent = NULL;
+      left0 = NULL;
+      right0 = NULL;
+    }
+
+    // deletes tree's child nodes
+    void deleteChilds(){
+      if(left0) left0->deleteChilds();
+      if(right1) right1->deleteChilds();
+
+      delete left0;
+      delete right1;
+    }
+
+    
+    void calculateNodeIDs(std::mutex& counter_mutex, int& counter){
+
+      {
+	std::lock_guard<std::mutex> lock(counter_mutex);
+	this->nodeid = counter;
+	counter++;
+      }
+      
+      if(left0) left0->calculateNodeIDs(counter_mutex, counter);
+      if(right1) right1->calculateNodeIDs(counter_mutex, counter);
+      
+    }
+
+    void saveData(std::vector<int>& data){
+      
+      if(left0) left0->saveData(data);
+      if(right1) right1->saveData(data);
+
+      data[this->nodeid*5 + 0] = this->nodeid;
+      data[this->nodeid*5 + 1] = this->decisionVariable;
+      data[this->nodeid*5 + 2] = this->outcome;
+
+      if(left0) data[this->nodeid*5 + 3] = left0->nodeid;
+      else data[this->nodeid*5 + 3] = -1;
+      
+      if(right0) data[this->nodeid*5 + 4] = right0->nodeid;
+      else data[this->nodeid*5 + 4] = -1;
+    }
+
+    bool loadData(std::vector<int>& data, int& counter){
+
+      this->nodeid = data[counter*5 + 0];
+      if(this->nodeid != counter) return false;
+      this->decisionVariable = data[counter*5 + 1];
+      this->outcome = data[counter*5 + 2];
+
+      int origcounter = counter;
+
+      if(data[counter*5 + 3] >= 0){
+
+	left0 = new DTNode();
+	left0->nodeid = data[counter*5 + 3];
+	
+	if(left0->loadData(data, ++counter) == false) return false;
+	if(left0->nodeid != data[origcounter*5 + 3]) return false;
+	
+      }
+
+      if(data[counter*5 + 4] >= 0){
+	
+	right1 = new DTNode();
+	right1->nodeid = data[counter*5 + 4];
+
+	if(right0->loadData(data, ++counter) == false) return false;
+	if(right0->nodeid != data[origcounter*5 + 4]) return false;
+	
+      }
+      
+      return true;
+    }
+    
+    
+    int decisionVariable;
+    std::set<int> variableSet;
+    int outcome; // leaf-node's outcome
+    int nodeid; // for saving the tree
+
+    class DTNode *parent;  
+    class DTNode *left0, *right1; // child nodes;
+  };
+
+  
 
   class DecisionTree
   {
   public:
-    DecisionTree(const unsigned int NUM_BIN_INPUTS, const unsigned int NUM_DISCRETE_OUTCOMES = 2);
+    DecisionTree();
     virtual ~DecisionTree();
 
-    bool startTrain(const std::vector< std::vector<bool> >& input, const std::vector< std::vector<bool> >& outcomes);
+    bool startTrain(const std::vector< std::vector<bool> >& inputs, const std::vector< std::vector<bool> >& outcomes);
     bool stopTrain();
     bool isRunning() const;
 
@@ -31,7 +128,17 @@ namespace whiteice
     
   private:
 
-    
+    // input data: pointers to const objects
+    std::vector< std::vector<bool> > const * inputs
+    std::vector< std::vector<bool> > const * outcomes;
+
+    // calculated decision tree
+    DTNode* tree;
+    mutable std::mutex tree_lock;
+
+    bool running;
+    std::thread* worker_thread;
+    std::mutex thread_mutex;
   };
 
   
