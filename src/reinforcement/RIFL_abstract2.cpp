aefc876ce8633bcd56faccbbbb04462e7005340c
Tomas Ukkonen <tomas.ukkonen@iki.fi>
2024-06-17 01:43:21 +0300

    Start learning from data already at 1000 samples, not 2000.

--- src/reinforcement/RIFL_abstract2.cpp
+++ src/reinforcement/RIFL_abstract2.cpp
@@ -878,7 +878,7 @@ namespace whiteice
     // assumes each episode length is 100 so this is ~ equal to 1.000.000 samples
     const unsigned long EPISODES_MAX_SIZE = 10000;
     const unsigned long MINIMUM_EPISODE_SIZE = 25;
-    const unsigned long MINIMUM_DATASIZE = 2000; // samples required to start learning, was:10000,2000,1000
+    const unsigned long MINIMUM_DATASIZE = 1000; // samples required to start learning, was:10000,2000,1000
     const unsigned long SAMPLESIZE = 2000; // number of samples used in learning, was: 5000,2000,1000 
     unsigned long database_counter = 0;
     unsigned long episodes_counter = 0;
